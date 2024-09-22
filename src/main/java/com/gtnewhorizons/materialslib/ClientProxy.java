7673f4fd1474231c54d0f8fce59061668136be34
serenibyss <10861407+serenibyss@users.noreply.github.com>
2024-09-22 16:49:04 -0500

    skeleton mod files

---
+++ src/main/java/com/gtnewhorizons/materialslib/ClientProxy.java
@@ -0,0 +1,5 @@
+package com.gtnewhorizons.materialslib;
+
+public class ClientProxy extends CommonProxy {
+
+}
---
+++ src/main/java/com/gtnewhorizons/materialslib/MaterialsLib.java
@@ -0,0 +1,48 @@
+package com.gtnewhorizons.materialslib;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(
+    modid = MaterialsLib.MODID,
+    version = Tags.VERSION,
+    name = MaterialsLib.MODNAME,
+    acceptedMinecraftVersions = "[1.7.10]")
+public class MaterialsLib {
+
+    public static final String MODID = "materialslib";
+    public static final String MODNAME = "MaterialsLib";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(
+        clientSide = "com.gtnewhorizons.materialslib.ClientProxy",
+        serverSide = "com.gtnewhorizons.materialslib.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
